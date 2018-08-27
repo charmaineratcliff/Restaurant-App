c9c3d5b4da756a6210bc6354fc3b7d4063d2850b
Charmaine Ratcliff <charmaineratcliff@gmail.com>
2018-08-27 00:19:21 -0400

    Changed the maps to Google maps

--- js/main.js
+++ js/main.js
@@ -69,26 +69,9 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize leaflet map, called from HTML.
+ * Initialize Google map, called from HTML.
  */
-initMap = () => {
-  self.newMap = L.map('map', {
-        center: [40.722216, -73.987501],
-        zoom: 12,
-        scrollWheelZoom: false
-      });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiY2hhcm1haW5lcmF0Y2xpZmYiLCJhIjoiY2psYm5ueWh0MmJmNDNxbnRjZXZkYm9vbyJ9.UYPIuAxJz9m1yXVhJnGwmQ',
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(newMap);
-
-  updateRestaurants();
-}
-/* window.initMap = () => {
+window.initMap = () => {
   let loc = {
     lat: 40.722216,
     lng: -73.987501
@@ -99,7 +82,7 @@ initMap = () => {
     scrollwheel: false
   });
   updateRestaurants();
-} */
+} 
 
 /**
  * Update page and map for current restaurants.
